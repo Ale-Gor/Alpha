c08068aed2781d217e4e4728eddb1cfb438f00da
AleGor <agorevalov@luxoft.com>
2019-01-18 12:13:50 +0100

    changes for b-2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("3");
+
         // b-3 created
         //new commit after that
 
+
+        // branch 2
 
 
     }
